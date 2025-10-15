39e1aadba4c50a78630f5619df595e814b590544
Steve Beale <steve@imagitech.co.uk>
2025-10-15 14:06:40 +0100

    Initial set-up, run out of time.

---
+++ Database/DataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BlatchAPI.Database
+{
+    public class DataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task InsertUsers()
+        {
+            AzureBlobService azureBlobService = new AzureBlobService();
+
+            var users = await azureBlobService.GetUsersAsync();
+
+            //Insert statement dapper
+            //How to handle addresses, skills, colleagues?
+
+            using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+
+            await connection.ExecuteAsync(users);
+        }
+    }
+}
